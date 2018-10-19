d10813bc8a34462285b0afc573b85c3cc233e33f
Shubham Rastogi <rastogi.s@husky.neu.edu>
2018-10-19 09:31:44 -0400

    rectified build error

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-console.log(path.join(__dirname, '/dist/reddit-web-app'));
-app.use(path.join(__dirname, '/dist/reddit-web-app'));
+app.use(express.static(path.join(__dirname, '/dist/reddit-web-app')));
 
 app.get('/*', function(req,res) {
-  console.log(path.join(__dirname,'/dist/reddit-web-app/index.html'));
-  //console.log(express.static(path.join(__dirname,'/dist/reddit-web-app/index.html')));
 
-  res.sendFile(path.join(__dirname,'/dist/reddit-web-app/index.html'));
+  res.sendFile(express.static(path.join(__dirname,'/dist/reddit-web-app/index.html')));
 });
 
 // Start the app by listening on the default Heroku port
