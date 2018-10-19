202df5bed5fda9e71d46fce91898acb47b2b0cfd
Shubham Rastogi <rastogi.s@husky.neu.edu>
2018-10-19 09:29:08 -0400

    rectified build error

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 console.log(path.join(__dirname, '/dist/reddit-web-app'));
-app.use(express.static(path.join(__dirname, '/dist/reddit-web-app')));
+app.use(path.join(__dirname, '/dist/reddit-web-app'));
 
 app.get('/*', function(req,res) {
   console.log(path.join(__dirname,'/dist/reddit-web-app/index.html'));
-  console.log(express.static(path.join(__dirname,'/dist/reddit-web-app/index.html')));
+  //console.log(express.static(path.join(__dirname,'/dist/reddit-web-app/index.html')));
 
   res.sendFile(path.join(__dirname,'/dist/reddit-web-app/index.html'));
 });
