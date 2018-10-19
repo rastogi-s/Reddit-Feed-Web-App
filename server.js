f49a50a15a692fa645af783e607798f2a85e698e
Shubham Rastogi <rastogi.s@husky.neu.edu>
2018-10-19 10:53:18 -0400

    rectified build error

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,12 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '/dist/reddit-web-app')));
+app.use(express.static(path.join(__dirname, '/dist/whiteboard-client-angular')));
 
 app.get('/*', function(req,res) {
 
- res.sendFile(express.static(path.join(__dirname+'/dist/reddit-web-app/index.html')));
-  //res.sendFile(__dirname +'/dist/reddit-web-app/index.html');
+  res.sendFile(express.static(path.join(__dirname,'/dist/whiteboard-client-angular/index.html')));
 });
 
 // Start the app by listening on the default Heroku port
